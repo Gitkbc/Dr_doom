bbba19edf46364df9b4d3df3134f4d8fa3aaadd9
Chaitanya Kalbhairav <chaitanyakalbhairav@gmail.com>
2024-12-25 11:00:24 +0530

    Initial commit of my project

--- android/app/src/main/kotlin/com/example/doom/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/doom/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.example.doom
+package com.your_package_name
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
